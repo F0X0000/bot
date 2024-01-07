b758cc82f73b0a61a58da41dab7d234530f83c73
Daniel Szewczuk <dsz8@pm.me>
2024-01-07 18:55:41 +0100

    Nowe komendy

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import socket
 from dotenv import load_dotenv
 import os
 from other import *
+from ping3 import ping as ping3_ping
+import datetime
+import requests
 
 load_dotenv()
 bot = discord.Bot()
@@ -15,7 +18,7 @@ async def on_ready():
 
 @bot.command(description="Wyświetla informacje o bocie.")
 async def help(ctx):
-    embed = discord.Embed(description="Wszystkie moje komendy znajdują się pod `/`")
+    embed = discord.Embed(description="Wszystkie moje komendy znajdują się pod `/` \n Ten bot jest open source, jego kod możesz zobaczyć [tutaj](https://github.com/szewczuko/bot)", color=discord.Colour.brand_green())
     await ctx.respond(embed = embed)
 
 @bot.command(name="8ball", description=" 🔮 Magiczna kula")
@@ -24,5 +27,26 @@ async def magiczna_kula(ctx, pytanie: discord.Option(str)):
     embed = discord.Embed(title=f"{odpowiedz}", color=discord.Colour.blurple())
     embed.set_author(name=f"{pytanie}", icon_url="https://em-content.zobj.net/source/microsoft/379/crystal-ball_1f52e.png")
     await ctx.respond(embed = embed)
+
+@bot.command(name="say", description="Mówi coś z konta bota.")
+async def say(ctx, wiadomosc: discord.Option(str)):
+    await ctx.respond(wiadomosc)
+
+@bot.command(name="weekend", description="Sprawdza czas do weekendu.")
+async def weekend(ctx):
+    now = datetime.datetime.now()
+    if now.weekday() == 4 and now.hour >= 16 or now.weekday() == 5 or now.weekday() == 6 or (now.weekday() == 0 and now.hour < 16):
+        await ctx.respond("Jest weekend! <:tada:1193546704729944084> ")
+    else:
+        days_until_weekend = (4 - now.weekday() + 7) % 7
+        hours_until_weekend = 16 - now.hour
+        minutes_until_weekend = 0 - now.minute
+        time_until_weekend = datetime.timedelta(days=days_until_weekend, hours=hours_until_weekend, minutes=minutes_until_weekend)
+        await ctx.respond(f"Do weekendu pozostało: {time_until_weekend}")
+
+@bot.command(name="poezja", description="Randomowy cytat z kanału #cytaty.")
+async def poezja(ctx):
+    odpowiedz = losuj("other/cytaty.txt")
+    await ctx.respond(odpowiedz)
 
 bot.run(os.getenv('TOKEN'))
